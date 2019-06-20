24fb527ff21eeffc264066a32220e4c39cd897be
aviad.hahami <aviad.hahami@cyiot.net>
2019-06-21 01:28:02 +0300

    structs

--- classes/Heat.js
+++ classes/Heat.js
@@ -1,21 +1,22 @@
 module.exports = class Heat{
-  constructor({name, time = 0, judgesCount, isJuniors = false, langs=[]}){
-    this.isJuniors = isJuniors
-    this.name = name;
-    this.time = time;
-    this.judgesCount = judgesCount;
-    this.langs = langs;
-    this.judges = [];
-    this.headJudge = undefined;
-    this.floorJudge = undefined;
-  }
+	constructor({name, time = 0, floorJudgesCount = 2, allowBadJudge = false, judgesCount, isJuniors = false, langs=[]}){
+		this.isJuniors = isJuniors;
+		this.name = name;
+		this.time = time;
+		this.judgesCount = judgesCount;
+		this.langs = langs;
+		this.judges = [];
+		this.headJudge = undefined;
+		this.floorJudge = [];
+		this.floorJudgeCount = floorJudgeCount;
+	}
 
 
-  get details(){
-    return {name: this.name, time: this.time}
-  }
+	get details(){
+		return {name: this.name, time: this.time}
+	}
 
-  get isFull(){
-    return (this.judgesCount === this.judges.length) && this.headJudge && this.floorJudge;
-  }
+	get isFull(){
+		return (this.judgesCount === this.judges.length) && this.headJudge && this.floorJudge;
+	}
 };
